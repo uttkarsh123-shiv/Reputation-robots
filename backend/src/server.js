1b3ef484a7b750b71267a4e7f3590c4f08b6da27
uttkarsh123-shiv <uttkarsh123.shiv@gmail.com>
2026-02-20 09:55:03 +0530

    Update the URL for CORS policy

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use(express.json());
